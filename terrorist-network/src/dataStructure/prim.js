d1279261ad6091cb7b328db1074ec0d9dec4676a
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-05 00:04:21 -0300

    Create populateGraph function and edit prim.js file.

--- terrorist-network/src/dataStructure/prim.js
+++ terrorist-network/src/dataStructure/prim.js
@@ -48,10 +48,10 @@ export class Graph {
     var edges = [];
 
     for (var node in this.nodes) {
-      for (var edge of this.nodes[edge]['neighbor']) {
+      for (var edge of this.nodes[node]['neighbor']) {
         var newEdge = {
           from: node,
-          to: edge.toString(),
+          to: edge,
         };
         edges.push(newEdge);
       }
@@ -132,75 +132,3 @@ export class Graph {
     console.log(this.nodes);
   }
 }
-
-const graph = new Graph();
-
-graph.addNode("Kelton Boaventura Natal");
-graph.addNode("Ezio Feijó Gaspar");
-graph.addNode("Ivo Freire Guilheiro");
-graph.addNode("Gonçalo Torrado Colares");
-graph.addNode("Esther Fernandes Cerqueira");
-graph.addNode("Édi Morais Domingues");
-graph.addNode("Zhen Galante Gama");
-graph.addNode("Kylian Belmonte Mantas");
-graph.addNode("Aya Cisneiros Valim");
-graph.addNode("Otília Covilhã Bilhalva");
-graph.addNode("Lya Fazendeiro Breia");
-graph.addNode("Teresa Amaral Adão");
-graph.addNode("Mafalda Borges Couceiro");
-graph.addNode("Pietra Alcântara Espinosa");
-graph.addNode("Isac Pescada Ferraz");
-graph.addNode("Daria Pinhal Craveiro");
-graph.addNode("Iúri Laranjeira Amorim");
-graph.addNode("Oscar Serpa Tavares");
-graph.addNode("Leila Sítima Lameiras");
-graph.addNode("Briana Madureira Matias");
-graph.addNode("Anaísa Gomide Rosário");
-
-graph.addEdge("Kelton Boaventura Natal", "Ezio Feijó Gaspar", 0)
-graph.addEdge("Kelton Boaventura Natal", "Ivo Freire Guilheiro", 0)
-graph.addEdge("Kelton Boaventura Natal", "Gonçalo Torrado Colares", 0)
-graph.addEdge("Ezio Feijó Gaspar", "Ivo Freire Guilheiro", 0)
-graph.addEdge("Ezio Feijó Gaspar", "Gonçalo Torrado Colares", 0)
-graph.addEdge("Ivo Freire Guilheiro", "Gonçalo Torrado Colares", 0)
-graph.addEdge("Ivo Freire Guilheiro", "Esther Fernandes Cerqueira", 0)
-graph.addEdge("Gonçalo Torrado Colares", "Esther Fernandes Cerqueira", 0)
-graph.addEdge("Gonçalo Torrado Colares", "Édi Morais Domingues", 0)
-graph.addEdge("Gonçalo Torrado Colares", "Zhen Galante Gama", 0)
-graph.addEdge("Gonçalo Torrado Colares", "Kylian Belmonte Mantas", 0)
-graph.addEdge("Esther Fernandes Cerqueira", "Édi Morais Domingues", 0)
-graph.addEdge("Esther Fernandes Cerqueira", "Zhen Galante Gama", 0)
-graph.addEdge("Édi Morais Domingues", "Aya Cisneiros Valim", 0)
-graph.addEdge("Édi Morais Domingues", "Otília Covilhã Bilhalva", 0)
-graph.addEdge("Zhen Galante Gama", "Kylian Belmonte Mantas", 0)
-graph.addEdge("Kylian Belmonte Mantas", "Aya Cisneiros Valim", 0)
-graph.addEdge("Kylian Belmonte Mantas", "Lya Fazendeiro Breia", 0)
-graph.addEdge("Aya Cisneiros Valim", "Otília Covilhã Bilhalva", 0)
-graph.addEdge("Aya Cisneiros Valim", "Teresa Amaral Adão", 0)
-graph.addEdge("Aya Cisneiros Valim", "Mafalda Borges Couceiro", 0)
-graph.addEdge("Aya Cisneiros Valim", "Lya Fazendeiro Breia", 0)
-graph.addEdge("Esther Fernandes Cerqueira", "Isac Pescada Ferraz", 0)
-graph.addEdge("Zhen Galante Gama", "Briana Madureira Matias", 0)
-graph.addEdge("Aya Cisneiros Valim", "Anaísa Gomide Rosário", 0)
-graph.addEdge("Otília Covilhã Bilhalva", "Anaísa Gomide Rosário", 0)
-graph.addEdge("Otília Covilhã Bilhalva", "Isac Pescada Ferraz", 0)
-graph.addEdge("Otília Covilhã Bilhalva", "Lya Fazendeiro Breia", 0)
-graph.addEdge("Lya Fazendeiro Breia", "Isac Pescada Ferraz", 0)
-graph.addEdge("Lya Fazendeiro Breia", "Ivo Freire Guilheiro", 0)
-graph.addEdge("Lya Fazendeiro Breia", "Édi Morais Domingues", 0)
-graph.addEdge("Lya Fazendeiro Breia", "Daria Pinhal Craveiro", 0)
-graph.addEdge("Daria Pinhal Craveiro", "Isac Pescada Ferraz", 0)
-graph.addEdge("Daria Pinhal Craveiro", "Teresa Amaral Adão", 0)
-graph.addEdge("Daria Pinhal Craveiro", "Iúri Laranjeira Amorim", 0)
-graph.addEdge("Teresa Amaral Adão", "Iúri Laranjeira Amorim", 0)
-graph.addEdge("Teresa Amaral Adão", "Ezio Feijó Gaspar", 0)
-graph.addEdge("Teresa Amaral Adão", "Lya Fazendeiro Breia", 0)
-graph.addEdge("Teresa Amaral Adão", "Oscar Serpa Tavares", 0)
-graph.addEdge("Teresa Amaral Adão", "Anaísa Gomide Rosário", 0)
-graph.addEdge("Teresa Amaral Adão", "Briana Madureira Matias", 0)
-graph.addEdge("Briana Madureira Matias", "Iúri Laranjeira Amorim", 0)
-graph.addEdge("Briana Madureira Matias", "Oscar Serpa Tavares", 0)
-graph.addEdge("Anaísa Gomide Rosário", "Leila Sítima Lameiras", 0)
-graph.addEdge("Anaísa Gomide Rosário", "Kelton Boaventura Natal", 0)
-graph.addEdge("Anaísa Gomide Rosário", "Ezio Feijó Gaspar", 0)
-graph.addEdge("Anaísa Gomide Rosário", "Mafalda Borges Couceiro", 0)
---
+++ terrorist-network/src/utils/populateGraph.js
@@ -0,0 +1,109 @@
+import { Graph } from '../dataStructure/prim.js';
+
+export const populateGraph = () => {
+  const generateTime = () => {
+    return Math.floor(Math.random() * 30);
+  }
+
+  const graph = new Graph();
+
+  // Adição dos nós (possíveis terroristas)
+  graph.addNode("Kelton Boaventura Natal");
+  graph.addNode("Ezio Feijó Gaspar");
+  graph.addNode("Ivo Freire Guilheiro");
+  graph.addNode("Gonçalo Torrado Colares");
+  graph.addNode("Esther Fernandes Cerqueira");
+  graph.addNode("Édi Morais Domingues");
+  graph.addNode("Zhen Galante Gama");
+  graph.addNode("Kylian Belmonte Mantas");
+  graph.addNode("Aya Cisneiros Valim");
+  graph.addNode("Otília Covilhã Bilhalva");
+  graph.addNode("Lya Fazendeiro Breia");
+  graph.addNode("Teresa Amaral Adão");
+  graph.addNode("Mafalda Borges Couceiro");
+  graph.addNode("Pietra Alcântara Espinosa");
+  graph.addNode("Isac Pescada Ferraz");
+  graph.addNode("Daria Pinhal Craveiro");
+  graph.addNode("Iúri Laranjeira Amorim");
+  graph.addNode("Oscar Serpa Tavares");
+  graph.addNode("Leila Sítima Lameiras");
+  graph.addNode("Briana Madureira Matias");
+  graph.addNode("Anaísa Gomide Rosário");
+
+  // Adição das arestas (tempo (em minutos) de ligação entre duas pessoas)
+  graph.addEdge("Kelton Boaventura Natal", "Ezio Feijó Gaspar", generateTime());
+  graph.addEdge("Kelton Boaventura Natal", "Ivo Freire Guilheiro", generateTime());
+  graph.addEdge("Kelton Boaventura Natal", "Gonçalo Torrado Colares", generateTime());
+  graph.addEdge("Ezio Feijó Gaspar", "Ivo Freire Guilheiro", generateTime());
+  graph.addEdge("Ezio Feijó Gaspar", "Gonçalo Torrado Colares", generateTime());
+  graph.addEdge("Ivo Freire Guilheiro", "Gonçalo Torrado Colares", generateTime());
+  graph.addEdge("Ivo Freire Guilheiro", "Esther Fernandes Cerqueira", generateTime());
+  graph.addEdge("Gonçalo Torrado Colares", "Esther Fernandes Cerqueira", generateTime());
+  graph.addEdge("Gonçalo Torrado Colares", "Édi Morais Domingues", generateTime());
+  graph.addEdge("Gonçalo Torrado Colares", "Zhen Galante Gama", generateTime());
+  graph.addEdge("Gonçalo Torrado Colares", "Kylian Belmonte Mantas", generateTime());
+  graph.addEdge("Esther Fernandes Cerqueira", "Édi Morais Domingues", generateTime());
+  graph.addEdge("Esther Fernandes Cerqueira", "Zhen Galante Gama", generateTime());
+  graph.addEdge("Édi Morais Domingues", "Aya Cisneiros Valim", generateTime());
+  graph.addEdge("Édi Morais Domingues", "Otília Covilhã Bilhalva", generateTime());
+  graph.addEdge("Zhen Galante Gama", "Kylian Belmonte Mantas", generateTime());
+  graph.addEdge("Kylian Belmonte Mantas", "Aya Cisneiros Valim", generateTime());
+  graph.addEdge("Kylian Belmonte Mantas", "Lya Fazendeiro Breia", generateTime());
+  graph.addEdge("Aya Cisneiros Valim", "Otília Covilhã Bilhalva", generateTime());
+  graph.addEdge("Aya Cisneiros Valim", "Teresa Amaral Adão", generateTime());
+  graph.addEdge("Aya Cisneiros Valim", "Mafalda Borges Couceiro", generateTime());
+  graph.addEdge("Aya Cisneiros Valim", "Lya Fazendeiro Breia", generateTime());
+  graph.addEdge("Esther Fernandes Cerqueira", "Isac Pescada Ferraz", generateTime());
+  graph.addEdge("Zhen Galante Gama", "Briana Madureira Matias", generateTime());
+  graph.addEdge("Aya Cisneiros Valim", "Anaísa Gomide Rosário", generateTime());
+  graph.addEdge("Otília Covilhã Bilhalva", "Anaísa Gomide Rosário", generateTime());
+  graph.addEdge("Otília Covilhã Bilhalva", "Isac Pescada Ferraz", generateTime());
+  graph.addEdge("Otília Covilhã Bilhalva", "Lya Fazendeiro Breia", generateTime());
+  graph.addEdge("Lya Fazendeiro Breia", "Isac Pescada Ferraz", generateTime());
+  graph.addEdge("Lya Fazendeiro Breia", "Ivo Freire Guilheiro", generateTime());
+  graph.addEdge("Lya Fazendeiro Breia", "Édi Morais Domingues", generateTime());
+  graph.addEdge("Lya Fazendeiro Breia", "Daria Pinhal Craveiro", generateTime());
+  graph.addEdge("Daria Pinhal Craveiro", "Isac Pescada Ferraz", generateTime());
+  graph.addEdge("Daria Pinhal Craveiro", "Teresa Amaral Adão", generateTime());
+  graph.addEdge("Daria Pinhal Craveiro", "Iúri Laranjeira Amorim", generateTime());
+  graph.addEdge("Teresa Amaral Adão", "Iúri Laranjeira Amorim", generateTime());
+  graph.addEdge("Teresa Amaral Adão", "Ezio Feijó Gaspar", generateTime());
+  graph.addEdge("Teresa Amaral Adão", "Lya Fazendeiro Breia", generateTime());
+  graph.addEdge("Teresa Amaral Adão", "Oscar Serpa Tavares", generateTime());
+  graph.addEdge("Teresa Amaral Adão", "Anaísa Gomide Rosário", generateTime());
+  graph.addEdge("Teresa Amaral Adão", "Briana Madureira Matias", generateTime());
+  graph.addEdge("Briana Madureira Matias", "Iúri Laranjeira Amorim", generateTime());
+  graph.addEdge("Briana Madureira Matias", "Oscar Serpa Tavares", generateTime());
+  graph.addEdge("Anaísa Gomide Rosário", "Leila Sítima Lameiras", generateTime());
+  graph.addEdge("Anaísa Gomide Rosário", "Kelton Boaventura Natal", generateTime());
+  graph.addEdge("Anaísa Gomide Rosário", "Ezio Feijó Gaspar", generateTime());
+  graph.addEdge("Anaísa Gomide Rosário", "Mafalda Borges Couceiro", generateTime());
+  graph.addEdge("Pietra Alcântara Espinosa", "Otília Covilhã Bilhalva", generateTime());
+
+  const getNodes = graph.getNodes();
+  const getEdges = graph.getEdges();
+  var allNodes = [];
+  var allEdges = [];
+
+  for (var node in getNodes) {
+    allNodes.push({
+      id: getNodes[node].id.split(' ')[0],
+      label: getNodes[node].id.split(' ')[0]
+    });
+  }
+
+  for (var edge in getEdges) {
+    allEdges.push({
+      from: getEdges[edge].to.source.split(' ')[0],
+      to: getEdges[edge].to.target.split(' ')[0],
+      label: 1
+    })
+  }
+
+  const graphFormated = {
+    nodes: allNodes,
+    edges: allEdges,
+  };
+
+  return graphFormated;
+};
