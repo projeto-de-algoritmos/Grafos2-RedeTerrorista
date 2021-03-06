3091bd5e6843a7099fffe983a59c12daf3967ea7
Fellipe Araujo <araujo.fellipe@hotmail.com>
2021-03-06 12:48:25 -0300

    Configure pages and edit styling.
    Co-authored-by: Ithalo Azevedo <ithaloazevedoo@gmail.com>

--- terrorist-network/src/App.js
+++ terrorist-network/src/App.js
@@ -1,54 +1,101 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Graph from 'react-graph-vis';
 import { populateGraph } from './utils/populateGraph';
-// import './styles.css';
-// import './network.css';
+import { formatGraphGeneral } from './utils/formatGraph';
+import { Graph } from './dataStructure/prim';
+import Targets from './pages/Targets';
+import TargetsConnection from './pages/TargetsConnection';
+import Home from './pages/Home';
 
 const App = () => {
-  const graph = populateGraph();
+  const graph = new Graph();
 
-  const options = {
-    autoResize: true,
-    height: '100%',
-    width: '100%',
-    nodes: {
-      borderWidth: 3,
-      size: 10,
-      color: {
-        border: '#222222',
-        background: '#ffff',
-        highlight: {
-          border: '#000080',
-          background: '#4682B4',
-        },
-      },
-      font: { color: '#2f2f2f' },
-    },
-    edges: {
-      color: '#f00',
-      arrows: {
-        to: {
-          enabled: false,
-        },
-        from: {
-          enabled: false,
-        },
-      },
-    },
+  const [changePage, setChangePage] = useState(0);
+  const [graphData, setGraphData] = useState(graph);
+  const [cost, setCost] = useState(0);
+
+  // const graphData = populateGraph(graph);
+  const graphdataFormated = formatGraphGeneral(graphData);
+  // const prim = graphData.prim('Anaísa Gomide Rosário');
+
+  const handlePopulateGraph = () => {
+    setGraphData(populateGraph(graph));
+    alert('Grafo populado');
   };
 
-  const events = {
-    select: function (event) {
-      // var { nodes, edges } = event;
-    },
+  const handleCalculateCost = () => {
+    setCost(graphData.prim('Anaísa Gomide Rosário').total);
   };
+
+  const renderPage = () => {
+    if (changePage === 0) {
+      return <Home />;
+    } else if (changePage === 1) {
+      return <Targets nodes={graphdataFormated.nodes} />;
+    } else if (changePage === 2) {
+      return <TargetsConnection edges={graphdataFormated.edges} />;
+    } else if (changePage === 3) {
+      return <h1>{cost}</h1>;
+    }
+  };
+
+  useEffect(() => {
+    renderPage();
+  }, [changePage]);
 
   return (
-    <div className="App">
-      <h1>Terrorist Network</h1>
-      <div className="graph-container">
-        <Graph graph={graph} options={options} events={events} />
+    <div id="app">
+      <div id="app-sidebar">
+        <div>
+          <h1 id="app-sidebar-title">Terrorist Network</h1>
+
+          <button
+            id="button-targets"
+            type="button"
+            onClick={() => setChangePage(0)}
+          >
+            Home
+          </button>
+
+          <button
+            id="button-targets"
+            type="button"
+            onClick={() => setChangePage(1)}
+          >
+            Alvos
+          </button>
+
+          <button
+            id="button-targets"
+            type="button"
+            onClick={() => setChangePage(2)}
+          >
+            Ligações entre Alvos
+          </button>
+
+          <button
+            id="button-targets"
+            type="button"
+            onClick={() => handlePopulateGraph()}
+          >
+            Popular Grafo
+          </button>
+
+          <button
+            id="button-targets"
+            type="button"
+            onClick={() => handleCalculateCost()}
+          >
+            Calcular menores ligações
+          </button>
+        </div>
+
+        <div id="app-sidebar-cost">
+          <h1 id="app-sidebar-cost-title">Menor custo (minutos)</h1>
+          <h2>{cost}</h2>
+        </div>
       </div>
+      <div id="app-content">{renderPage()}</div>
     </div>
   );
 };
